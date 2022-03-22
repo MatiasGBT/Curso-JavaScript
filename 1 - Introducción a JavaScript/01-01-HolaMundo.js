81be910d298eb91e59cf03045bb8a6ed9f4c6366
Matías Gabriel Blanco Toledo <blancotoledomatiasgabriel@gmail.com>
2022-03-22 10:57:04 -0300

    Agrego primera lección

---
+++ 1 - Introducción a JavaScript/01-01-HolaMundo.js
@@ -0,0 +1,6 @@
+//Esto es una variable en JavaScript
+var nombre = "Hola Mundo";
+//Estas pueden cambiar su tipo de dato dinámicamente simplemente asignandole otro valor:
+//nombre = 2;
+
+console.log(nombre);
